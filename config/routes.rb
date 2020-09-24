7b73985b680efb13d155871ef9f5ce7a38cb5c43
Travis Gandy <travis_gandy@yahoo.com>
2020-09-24 14:38:48 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :genres
-  resources :songs
+  resources :artists, :genres, :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
